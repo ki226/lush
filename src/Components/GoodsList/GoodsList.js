81c4f219e6226c092ee49c619ab095c22208dd74
hogni-seoul <hogni@kakao.com>
2020-06-24 14:25:30 +0900

    Goods, GoodsList 컴포넌트 / Product 페이지 작성

--- src/Components/GoodsList/GoodsList.js
+++ src/Components/GoodsList/GoodsList.js
@@ -1,12 +1,49 @@
 import React, { Component } from "react";
+import Goods from "Components/Goods";
 import "./GoodsList.scss";
 
 class GoodsList extends Component {
-  state = {};
   render() {
+    const { category_name, category_id, sub_category, results } = this.props;
+
     return (
       <div className="GoodsList">
-        <div>GoodsList</div>
+        <div className="heading">
+          <h2 className="category-name">{category_name}</h2>
+        </div>
+        <ul className="lower-category">
+          {sub_category.map((category) => (
+            <li key={category.category_id}>
+              <span>
+                <a
+                  href={`https://lush.co.kr/goods/goods_list.php?cateCd=${category.category_id}`}
+                  className={category.category_id === category_id ? "on" : null}
+                >
+                  {category.category_id.length === 6
+                    ? "전체"
+                    : category.category_name}
+                  {`(${category.goods_count})`}
+                </a>
+              </span>
+            </li>
+          ))}
+        </ul>
+        <div className="goods-list">
+          {results.map((result) => (
+            <Goods
+              key={result.goods_no}
+              goods_no={result.goods_path}
+              goods_name={result.goods_name}
+              goods_path={result.goods_path}
+              thumbnail_path={result.thumbnail_path}
+              goods_price={result.goods_price}
+              sold_out={result.sold_out}
+              latest={result.latest}
+              vegan={result.vegan}
+              tags={result.tags}
+            />
+          ))}
+        </div>
       </div>
     );
   }
