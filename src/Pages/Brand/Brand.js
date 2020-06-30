903870f87885b9ab1acab53396170391e88c39fe
ki226 <tkddn088@gmail.com>
2020-06-30 18:46:46 +0900

    배너 컴포넌트가 적용되는 각 페이지들은 각각의 브런치에서 말씀해주신 부분들 수정해서 올리겠습니다.

--- src/Pages/Brand/Brand.js
+++ src/Pages/Brand/Brand.js
@@ -1,25 +1,10 @@
 import React, { Component } from "react";
-import HeroBanner from "Components/HeroBanner";
 import "./Brand.scss";
 
 class Brand extends Component {
   state = {};
   render() {
-    return (
-      <div className="Brand">
-        <HeroBanner
-          banner={"lushBanner"}
-          bannerSubheading={"ABOUT LUSH"}
-          bannerTitle={"영국 프레쉬 핸드메이드 코스메틱 러쉬"}
-          bannerText1={
-            "러쉬는 자연으로부터 얻은 신선한 재료와 동물실험을 하지 않은 정직한 재료를 사용하여 모든 제품을 손으로 직접 만듭니다."
-          }
-          bannerText2={
-            "더불어 공정하게 얻은 원재료, 인권 보호, 포장 최소화 등 다양한 캠페인 활동을 통해 기업 윤리와 신념을 알리고 있습니다."
-          }
-        ></HeroBanner>
-      </div>
-    );
+    return <div className="Brand"></div>;
   }
 }
 
--- src/Pages/Spa/Spa.js
+++ src/Pages/Spa/Spa.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
 import "./Spa.scss";
-import HeroBanner from "Components/HeroBanner";
 
 class Spa extends Component {
   state = {};
   render() {
-    return (
-      <div className="Spa">
-        <HeroBanner banner={"spaBanner"}></HeroBanner>
-      </div>
-    );
+    return <div className="Spa"></div>;
   }
 }
 
