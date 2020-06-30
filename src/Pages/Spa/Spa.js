3bd64c69af4da806be35fbe0ffd96653aefc31cf
ki226 <tkddn088@gmail.com>
2020-06-30 15:39:11 +0900

    spa 페이지 상단 메뉴 구현

--- src/Pages/Spa/Spa.js
+++ src/Pages/Spa/Spa.js
@@ -6,7 +6,21 @@ class Spa extends Component {
   render() {
     return (
       <div className="Spa">
-        <div>Spa</div>
+        <div className="spa-head">
+          <div className="about-spa">
+            <div className="about">ABOUT</div>
+            <div className="voucher-guide">
+              스파 바우처 <br /> 이용방법
+            </div>
+            <div className="about-voucher">
+              스파 바우처에
+              <br /> 대해 알아보세요!
+            </div>
+            <div className="more-info">자세히 보기></div>
+          </div>
+          <div className="spa-store1"></div>
+          <div className="spa-store2"></div>
+        </div>
       </div>
     );
   }
